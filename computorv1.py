ed21c2e99c0413f75da726407f6c4ff543140af4
arthur laine <arthur.laine99@gmail.com>
2020-09-04 16:38:43 +0200

    added delta calcul and solutions calcul, improvments for degree 1 needed tho

--- computorv1.py
+++ computorv1.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 class Equation():
     def __init__(self, argv):
@@ -14,7 +15,46 @@ class Equation():
     def main(self, argv):
         self.check_degre(argv)
         self.get_red_eq(argv)
-        print("degre = {}\nsolvable = {}".format(self.degre, self.solvable))
+        if self.solvable is True:
+            self.calcul_delta()
+            if self.delta >= 0:
+                self.calcul_sols()
+        self.print_result()
+
+    def print_result(self):
+        print("Reduced form: {}\nPolynomial degree: {}".format(self.red_eq, self.degre))
+        if self.solvable is False:
+            """degre 0 ???"""
+            if self.degre > 2:
+                if self.delta < 0:
+                    print("The polynomial degree is strictly greater than 2, I can't solve.\nAnd the discriminant is strictly negative.")
+                else:
+                    print("The polynomial degree is strictly greater than 2, I can't solve.")
+            elif self.delta < 0:
+                print("The discriminant is striclty negative, there is no solutions.")
+        elif self.solvable is True:
+            if self.delta > 0:
+                print("Discriminant is strictly positive, the two solutions are:\n{:.6}\n{:.6}".format(self.sols[0], self.sols[1]))
+            elif self.delta == 0:
+                print("Discriminant equals zero, the solution is:\n{:.6}".format(self.sols[0]))
+
+    def calcul_delta(self):
+        for i in range(len(self.eq_elems)):
+            self.eq_elems[i] = self.eq_elems[i] * self.eq_signs[i]
+        if self.degre == 2:
+            self.delta = self.eq_elems[1] * self.eq_elems[1] - 4 * (self.eq_elems[0] * self.eq_elems[2])
+        """
+        delta for degree 1
+        """
+
+    def calcul_sols(self):
+        """solutions for degree 1 to do without delta"""
+        if self.degre == 1:
+            self.sols.append(((-1 * self.eq_elems[0]) - self.delta ** 0.5) / (2 * self.eq_elems[1]))
+        elif self.degre == 2:
+            self.sols.append(((-1 * self.eq_elems[1]) - self.delta ** 0.5) / (2 * self.eq_elems[2]))
+            if self.delta > 0:
+                self.sols.append(((-1 * self.eq_elems[1]) + self.delta ** 0.5) / (2 * self.eq_elems[2]))
 
     def check_degre(self, argv):
         i = len(argv) - 1
@@ -48,8 +88,6 @@ class Equation():
             self.red_eq += split_eq[0][i]
             i += 1
         self.red_eq += '= 0'
-        print(self.red_eq)
-        print(self.eq_elems, self.eq_signs)
 
     def loop_cast_elems_eq(self):
         for i in range(len(self.eq_elems)):
@@ -90,13 +128,14 @@ class Equation():
         i += 2
         elem, i = self.get_elem(split_eq, i)
         self.eq_elems.append(elem)
-        i += 7
-        if split_eq[i] == '-':
-            self.eq_signs.append(-1)
-        else:
-            self.eq_signs.append(1)
-        elem, i = self.get_elem(split_eq, i + 2)
-        self.eq_elems.append(elem)
+        if self.degre >= 2:
+            i += 7
+            if split_eq[i] == '-':
+                self.eq_signs.append(-1)
+            else:
+                self.eq_signs.append(1)
+            elem, i = self.get_elem(split_eq, i + 2)
+            self.eq_elems.append(elem)
 
     def get_elem(self, split_eq, i):
         elem = ''
