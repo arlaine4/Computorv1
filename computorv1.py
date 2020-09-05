592130020e91b5bbe10ece6846d248fe96f106c5
arthur laine <arthur.laine99@gmail.com>
2020-09-05 18:35:01 +0200

    fixed degree 0

--- computorv1.py
+++ computorv1.py
@@ -15,16 +15,25 @@ class Equation():
     def main(self, argv):
         self.check_degre(argv)
         self.get_red_eq(argv)
-        if self.solvable is True:
-            self.calcul_delta()
-            if self.delta >= 0:
-                self.calcul_sols()
-        self.print_result()
+        if self.solvable is True or self.degre == 0:
+            if self.degre == 0:
+                self.process_degre_zero()
+            else:
+                self.calcul_delta()
+                if self.delta >= 0:
+                    self.calcul_sols()
+        if self.degre != 0:
+            self.print_result()
+
+    def process_degre_zero(self):
+        if self.eq_elems[0] == 0:
+            print("All real numbers are a solution.")
+        else:
+            print("This equation is impossible to solve.")
 
     def print_result(self):
         print("Reduced form: {}\nPolynomial degree: {}".format(self.red_eq, self.degre))
         if self.solvable is False:
-            """degre 0 ???"""
             if self.degre > 2:
                 if self.delta < 0:
                     print("The polynomial degree is strictly greater than 2, I can't solve.\nAnd the discriminant is strictly negative.")
@@ -33,10 +42,13 @@ class Equation():
             elif self.delta < 0:
                 print("The discriminant is striclty negative, there is no solutions.")
         elif self.solvable is True:
-            if self.delta > 0:
-                print("Discriminant is strictly positive, the two solutions are:\n{:.6}\n{:.6}".format(self.sols[0], self.sols[1]))
-            elif self.delta == 0:
-                print("Discriminant equals zero, the solution is:\n{:.6}".format(self.sols[0]))
+            if self.degre == 1:
+                print("The solution is:\n{}".format(self.sols[0]))
+            elif self.degre == 2:
+                if self.delta > 0:
+                    print("Discriminant is strictly positive, the two solutions are:\n{:.6}\n{:.6}".format(self.sols[0], self.sols[1]))
+                elif self.delta == 0:
+                    print("Discriminant equals zero, the solution is:\n{:.6}".format(self.sols[0]))
 
     def calcul_delta(self):
         for i in range(len(self.eq_elems)):
@@ -46,7 +58,10 @@ class Equation():
 
     def calcul_sols(self):
         if self.degre == 1:
-            self.sols.append(self.eq_elems[0] / self.eq_elems[1] * -1)
+            if self.eq_elems[1] == 0:
+                self.sols.append(0)
+            else:
+                self.sols.append(self.eq_elems[0] / self.eq_elems[1] * -1)
         elif self.degre == 2:
             self.sols.append(((-1 * self.eq_elems[1]) - self.delta ** 0.5) / (2 * self.eq_elems[2]))
             if self.delta > 0:
@@ -61,36 +76,71 @@ class Equation():
         if self.degre < 1 or self.degre > 2:
             self.solvable = False
 
+    def get_degree_zero(self, split_eq):
+        i = 0
+        last_elem = ''
+        first_elem = ''
+        if split_eq[0][i] == '-':
+            self.eq_signs.append(-1)
+            i += 2
+        else:
+            self.eq_signs.append(1)
+        first_elem, i = self.get_elem(split_eq[0], i)
+        i = 1
+        if split_eq[1][i] == '-':
+            self.eq_signs.append(-1)
+            i += 2
+        else:
+            self.eq_signs.append(1)
+        last_elem, i = self.get_elem(split_eq[1], i)
+        first_elem, last_elem = self.loop_cast_elems_eq(first_elem, last_elem)
+        first_elem *= self.eq_signs[0] ; last_elem *= self.eq_signs[1]
+        self.eq_elems.append(first_elem - last_elem)
+
     def get_red_eq(self, argv):
         split_eq = argv.split('=')
-        self.get_elems_and_signs(split_eq[0])
-        first_elem, sign_first_elem = self.get_first_elem(split_eq[1])
-        if '.' in first_elem:
-            first_elem = float(first_elem)
+        if self.degre == 0:
+            self.get_degree_zero(split_eq)
         else:
-            first_elem = int(first_elem)
-        if sign_first_elem == '-':
-            first_elem *= -1
-        self.loop_cast_elems_eq()
-        self.eq_elems[0] = self.eq_elems[0] * self.eq_signs[0] - first_elem
-        if self.eq_elems[0] < 0:
-            self.red_eq += '- {}'.format(self.eq_elems[0] * -1)
-        else:
-            self.red_eq += str(self.eq_elems[0])
-        i = 2 + (len(str(self.eq_elems[0])) - 2)
-        if self.eq_signs[0] == -1:
-            i += 1
-        while i < len(split_eq[0]):
-            self.red_eq += split_eq[0][i]
-            i += 1
-        self.red_eq += '= 0'
-
-    def loop_cast_elems_eq(self):
-        for i in range(len(self.eq_elems)):
-            if '.' in self.eq_elems[i]:
-                self.eq_elems[i] = float(self.eq_elems[i])
+            self.get_elems_and_signs(split_eq[0])
+            first_elem, sign_first_elem = self.get_first_elem(split_eq[1])
+            if '.' in first_elem:
+                first_elem = float(first_elem)
             else:
-                self.eq_elems[i] = int(self.eq_elems[i])
+                first_elem = int(first_elem)
+            if sign_first_elem == '-':
+                first_elem *= -1
+            self.loop_cast_elems_eq()
+            self.eq_elems[0] = self.eq_elems[0] * self.eq_signs[0] - first_elem
+            if self.eq_elems[0] < 0:
+                self.red_eq += '- {}'.format(self.eq_elems[0] * -1)
+            else:
+                self.red_eq += str(self.eq_elems[0])
+            i = 2 + (len(str(self.eq_elems[0])) - 2)
+            if self.eq_signs[0] == -1:
+                i += 1
+            while i < len(split_eq[0]):
+                self.red_eq += split_eq[0][i]
+                i += 1
+            self.red_eq += '= 0'
+
+    def loop_cast_elems_eq(self, elem1=None, elem2=None):
+        if elem1 is not None:
+            if '.' in elem1:
+                elem1 = float(elem1)
+            else:
+                elem1 = int(elem1)
+            if '.' in elem2:
+                elem2 = float(elem2)
+            else:
+                elem2 = int(elem2)
+            return elem1, elem2
+        else:
+            for i in range(len(self.eq_elems)):
+                if '.' in self.eq_elems[i]:
+                    self.eq_elems[i] = float(self.eq_elems[i])
+                else:
+                    self.eq_elems[i] = int(self.eq_elems[i])
 
     def get_first_elem(self, split_eq):
         elem = ''
